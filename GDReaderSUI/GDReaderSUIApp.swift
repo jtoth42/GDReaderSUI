4b7b18551b0c056f012fe24ccc0da466fe1cbfd3
Jim <jamestoth@q.com>
2022-05-25 19:54:35 -0700

    Initial Commit

---
+++ GDReaderSUI/GDReaderSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDReaderSUIApp.swift
+//  GDReaderSUI
+//
+//  Created by Jim on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct GDReaderSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
