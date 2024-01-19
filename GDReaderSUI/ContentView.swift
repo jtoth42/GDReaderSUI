47473850b53198ce460f9c225b9c8a6254198c55
Jim <jamestoth@q.com>
2024-01-19 13:35:41 -0700

    Remove NavigationViews, replace with DIY master-detail navigation. Add/improve documentation and formatting.

--- GDReaderSUI/ContentView.swift
+++ GDReaderSUI/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  GDReaderSUI
 //
-//  Created by Jim Toth May 2022, last updated July 2022.
+//  Created by Jim Toth May 2022, last updated January 2024.
 //
 
 import SwiftUI
@@ -27,46 +27,64 @@ struct ContentView: View {
   0 @N1@ SNOTE Shared note 1
   0 TRLR
   """  // Placeholder GEDCOM File
+
+// GeneralRecord is anything other than a Family or Individual Record
   
-  struct Family: Identifiable, Comparable {
+  struct Family: Identifiable, Comparable, Hashable {
     let id: String
     var husb: String = "?"
     var wife: String = "?"
     let textContent: String
     static func < (lhs: Family, rhs: Family) -> Bool { lhs.id.localizedStandardCompare(rhs.id) == .orderedAscending }
   }
-  @State private var families = [
-    Family(id: "F1", textContent: "FAM\n1 HUSB @I1@\n1 WIFE @I2@")
-  ]  // Placeholders, from the placeholder GEDCOM file.
    
-  struct Individual: Identifiable {
+  struct Individual: Identifiable, Comparable, Hashable {
     let id: String
     let name: String
     let textContent: String
+    static func < (lhs: Individual, rhs: Individual) -> Bool {
+      lhs.id.localizedStandardCompare(rhs.id) == .orderedAscending
+    }
   }
-  @State private var individuals = [
-    Individual(id: "I1", name: "Smith John", textContent: "INDI\n1 NAME John /Smith/\n1 FAMS @F1@"),
-    Individual(id: "I2", name: "Doe Jane", textContent: "INDI\n1 NAME Jane /Doe/\n1 FAMS @F1@")
-  ]  // Placeholders, from the placeholder GEDCOM file.
-  @State private var indKeyPath: KeyPath = \Individual.id  // Default for the Picker in the body below.
-
-  struct GeneralRecord: Identifiable, Comparable {
+  
+  struct GeneralRecord: Identifiable, Comparable, Hashable {
     let id: String
     let textContent: String
     static func < (lhs: GeneralRecord, rhs: GeneralRecord) -> Bool { lhs.id.localizedStandardCompare(rhs.id) == .orderedAscending }
   }
+ 
+// The four record arrays are initialized mostly as if the placeholder file above had already been processed.
+
+  @State private var individuals = [
+    Individual(id: "I1", name: "Smith John", textContent: "INDI\n1 NAME John /Smith/\n1 FAMS @F1@"),
+    Individual(id: "I2", name: "Doe Jane", textContent: "INDI\n1 NAME Jane /Doe/\n1 FAMS @F1@")
+  ]
+  
+  @State private var families = [
+    Family(id: "F1", textContent: "FAM\n1 HUSB @I1@\n1 WIFE @I2@")
+  ]
+  
   @State private var sources = [GeneralRecord(id: "S1", textContent: "SOUR\n1 TITL Source One")]
+  
   @State private var others = [GeneralRecord(id: "HEAD", textContent: "1 GEDC\n2 VERS 7.0"),
                                GeneralRecord(id: "N1", textContent: "SNOTE Shared note 1\n0 TRLR")]
-  @State private var currentFileName: String = "click Open New"  // All Placeholders
   
+  @State private var indKeyPath: KeyPath = \Individual.id  // Default for the Picker in the body below.
+  
+  @State private var currentFileName: String = "click Open New"
+  
+  @State private var selectedFamily: Family?
+  @State private var selectedIndividual: Individual?
+  @State private var selectedSource: GeneralRecord?
+  @State private var selectedOther: GeneralRecord?
+
   var body: some View {
     HStack {  // Three vertical stacks, side by side
       VStack (alignment: .leading) {
         Text(currentFileName)
         TextEditor(text: .constant(text))
           .font(.body)
-        HStack() {
+        HStack {
           Button(action: {
             let openURL = showOpenPanel()
             readText(from: openURL)
@@ -78,7 +96,11 @@ struct ContentView: View {
             .frame(width: 120)
           })
           Button(action: {
-              processText()
+            selectedFamily = nil
+            selectedIndividual = nil
+            selectedSource = nil
+            selectedOther = nil
+            processText()
           }, label: {
             Text("Process")
             .frame(width: 80)
@@ -98,58 +120,65 @@ struct ContentView: View {
       } // end of VStack One Closure
       .frame(width: 300.0)
       VStack (alignment: .leading) {
-        HStack(){
+        HStack {
           Text(" FAM")
             .frame(minWidth: 50.0)
           Text("HUSB/WIFE")
         }
-        NavigationView {
-          List() {
-            ForEach(families.sorted()) { Family in
-              NavigationLink(destination: TextEditor(text: .constant(Family.textContent))) {
-                Text(Family.id)
-                Text(Family.husb)
-                Text(Family.wife)
-              }
+        HStack {
+          List(families.sorted(), id: \.self, selection: $selectedFamily) { Family in
+            HStack() {
+              Text(Family.id)
+              Text(Family.husb)
+              Text(Family.wife)
             }
           }
-          Text("No selection")
+          if let selectedFamily {
+            TextEditor(text: .constant( selectedFamily.textContent))
+          } else {
+            Text("No selection")
+          }
         }
-        HStack() {
+        HStack {
           Text("  INDI")
             .frame(width: 50.0)
           Text("Name")
         }
-        NavigationView {
-          List() {
-            ForEach(individuals.sorted(by: {$0[keyPath: indKeyPath].localizedStandardCompare($1[keyPath: indKeyPath]) == .orderedAscending})) { Individual in
-              NavigationLink(destination: TextEditor(text: .constant(Individual.textContent))) {
-                Text(Individual.id)
-                Text(Individual.name)
-              }
+        HStack {
+          List(individuals.sorted(by: {$0[keyPath: indKeyPath].localizedStandardCompare($1[keyPath: indKeyPath]) == .orderedAscending}), id: \.self, selection: $selectedIndividual) { Individual in
+            HStack {
+              Text(Individual.id)
+              Text(Individual.name)
             }
           }
-          Text("No selection")
-       }
+          if let selectedIndividual { TextEditor(text: .constant(selectedIndividual.textContent))
+          } else {
+            Text("No selection")
+          }
+        }
       } // end of VStack Two Closure
       VStack(alignment: .leading) {
         Text("SOUR")
-        NavigationView {
-          List {
-            ForEach(sources.sorted()) { GeneralRecord in
-              NavigationLink(GeneralRecord.id, destination: TextEditor(text: .constant( GeneralRecord.textContent)))
-            }
-           }
-          Text("No selection")
+        HStack {
+          List (sources.sorted(), id: \.self, selection: $selectedSource) { GeneralRecord in
+            Text(GeneralRecord.id)
+          }
+          if let selectedSource {
+            TextEditor(text: .constant( selectedSource.textContent))
+          } else {
+            Text("No selection")
+          }
         }
         Text("Other")
-        NavigationView {
-          List {
-            ForEach(others.sorted()) { GeneralRecord in
-              NavigationLink(GeneralRecord.id, destination: TextEditor(text: .constant( GeneralRecord.textContent)))
-            }
+        HStack {
+          List (others.sorted(), id: \.self, selection: $selectedOther) { GeneralRecord in
+            Text(GeneralRecord.id)
           }
-          Text("No selection")
+          if let selectedOther {
+            TextEditor(text: .constant( selectedOther.textContent))
+          } else {
+            Text("No selection")
+          }
         }
       } // end of VStack Three Closure
     } // end of HStack
@@ -179,16 +208,11 @@ struct ContentView: View {
   }
   
   func processText() {
-//
-// Splits "text" into GEDCOM records, placing each record into one
-// of four bins. Some additional processing is done. The header record
-// tag, HEAD, and content are destined for the default bin, Others.
-// The trailer record has no content. Its level and its TRLR tag
-// end up appended to the textContent of the final data record.
-// The while loop begins each pass by reading the entire content
-// of the current record, having already parsed the current
-// xRef on the previous pass through the loop.
-//
+
+// Splits "text" into GEDCOM records, placing each record into one of four bins. Some additional processing is done. The header record content, tagged HEAD, is destined for the default bin, Others.
+// The trailer record has no content. Its level and its TRLR tag end up appended to the textContent of the final data record.
+// The while loop begins each pass by reading the entire content of the current record, having already parsed the current xRef on the previous pass through the loop.
+
     var localFamilies: [Family] = []
     var localIndividuals: [Individual] = []
     var surnameDict: [String: String] = [:]
@@ -209,9 +233,8 @@ struct ContentView: View {
       case "F":
         localFamilies.append(Family(id: xRef, textContent: textContent))
       case "I":
-// For this case, an individual record, there is extra work to generate the name.
-// Parse the individual name, rearrange with surname first, save surname in
-// its own dict for later use.
+// For this case, an individual record, there is extra ugly code to generate the name.
+// Parse the individual name, rearrange with surname first, save surname in its own dict for later use.
 // Function restOfLineFrom is defined in this project's StringExtension.swift.
         var theName = "?"
         if let nameValue = textContent.restOfLineFrom("1 NAME ") {
